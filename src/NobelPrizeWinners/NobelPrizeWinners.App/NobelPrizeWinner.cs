21a4e2073276623d3b4d3b9f2b63c00994e3dcb2
ShadyObeyd <obeyd.shady@gmail.com>
2019-09-03 16:46:38 +0300

    App can now successfully read data from excel file and create NobelPrizeWinner model.

---
+++ src/NobelPrizeWinners/NobelPrizeWinners.App/NobelPrizeWinner.cs
@@ -0,0 +1,39 @@
+﻿namespace NobelPrizeWinners.App
+{
+    public class NobelPrizeWinner
+    {
+        public NobelPrizeWinner(string year, string category, string name, string birthdate, string birthplace, string country, string residence,
+            string fieldOrLanguage, string prizeName, string motivation)
+        {
+            this.Year = year;
+            this.Category = category;
+            this.Name = name;
+            this.Birthdate = birthdate;
+            this.BirthPlace = birthplace;
+            this.Country = country;
+            this.Residence = residence;
+            this.FieldOrLanguage = fieldOrLanguage;
+            this.PrizeName = prizeName;
+            this.Motivation = motivation;
+        }
+        public string Year { get; set; }
+
+        public string Category { get; set; }
+
+        public string Name { get; set; }
+
+        public string Birthdate { get; set; }
+
+        public string BirthPlace { get; set; }
+
+        public string Country { get; set; }
+
+        public string Residence { get; set; }
+
+        public string FieldOrLanguage { get; set; }
+
+        public string PrizeName { get; set; }
+
+        public string Motivation { get; set; }
+    }
+}
---
+++ src/NobelPrizeWinners/NobelPrizeWinners.App/StartUp.cs
@@ -0,0 +1,73 @@
+﻿namespace NobelPrizeWinners.App
+{
+    using OfficeOpenXml;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public static class StartUp
+    {
+        public static void Main()
+        {
+            string filePath = @""; // <-- Add file path
+
+            FileInfo file = new FileInfo(filePath);
+
+            List<NobelPrizeWinner> winners = new List<NobelPrizeWinner>();
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                int rowsCount = worksheet.Dimension.Rows;
+                int colsCount = worksheet.Dimension.Columns;
+
+                for (int row = 2; row <= 2; row++)
+                {
+                    string rawText = string.Empty;
+
+                    for (int col = 1; col <= colsCount; col++)
+                    {
+                        try
+                        {
+                            rawText += worksheet.Cells[row, col].Value.ToString() + "@";
+                        }
+                        catch (NullReferenceException)
+                        {
+                            rawText += "N/A@";
+                        }
+                    }
+                    string[] winnerTokens = rawText.Split('@', StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim()).ToArray();
+
+                    string year = winnerTokens[0];
+                    string category = winnerTokens[1];
+                    string name = winnerTokens[2];
+                    string birthdate = winnerTokens[3];
+                    string birthPlace = winnerTokens[4];
+                    string country = winnerTokens[5];
+                    string residence = winnerTokens[6];
+                    string fieldOrLanguage = winnerTokens[7];
+                    string prizeName = winnerTokens[8];
+                    string motivation = winnerTokens[9];
+
+                    NobelPrizeWinner winner = new NobelPrizeWinner(year, category, name, birthdate, birthPlace, country, residence, fieldOrLanguage, prizeName, motivation);
+                    winners.Add(winner);
+                }
+            }
+
+            var firstWinner = winners[0];
+
+            Console.WriteLine(firstWinner.Year);
+            Console.WriteLine(firstWinner.Category);
+            Console.WriteLine(firstWinner.Name);
+            Console.WriteLine(firstWinner.Birthdate);
+            Console.WriteLine(firstWinner.BirthPlace);
+            Console.WriteLine(firstWinner.Country);
+            Console.WriteLine(firstWinner.Residence);
+            Console.WriteLine(firstWinner.FieldOrLanguage);
+            Console.WriteLine(firstWinner.PrizeName);
+            Console.WriteLine(firstWinner.Motivation);
+        }
+    }
+}
